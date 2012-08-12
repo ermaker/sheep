668e094180f46b572ea0200a14abc2ed28b6e571
Minwoo Lee <ermaker@gmail.com>
2012-08-12 15:18:39 +0900

    Add Sheep#load

--- lib/sheep.rb
+++ lib/sheep.rb
@@ -1,2 +1,20 @@
 class Sheep
+  class NUMBEROF_OBJECTS_NOT_MATCHED < Exception; end
+  class NUMBEROF_POINTS_NOT_MATCHED < Exception; end
+
+  attr_reader :objects
+
+  def load filename
+    open(filename) do |file|
+      numberof_objects = file.readline.to_i
+      @objects = file.each_line.map do |line|
+        numberof_points, *points = line.split
+        numberof_points = numberof_points.to_i
+        points = points.map(&:to_f).each_slice(2).to_a
+        raise NUMBEROF_POINTS_NOT_MATCHED unless numberof_points == points.size
+        points
+      end
+      raise NUMBEROF_OBJECTS_NOT_MATCHED unless numberof_objects == @objects.size
+    end
+  end
 end
--- spec/sheep_spec.rb
+++ spec/sheep_spec.rb
@@ -1,4 +1,40 @@
 require 'sheep'
 
+def path filename
+  File.join(File.dirname(__FILE__), 'fixtures', filename)
+end
+
 describe Sheep do
+  context '#load' do
+    it 'reads map with 1.map' do
+      map = path('1.map')
+      subject.load map
+      subject.objects.should == []
+    end
+    it 'reads map with 2.map' do
+      map = path('2.map')
+      subject.load map
+      subject.objects.should == [[[1.0,1.0],[1.0,2.0],[2.0,2.0],[2.0,1.0]]]
+    end
+    it 'reads map with 3.map' do
+      map = path('3.map')
+      subject.load map
+      subject.objects.should == [
+        [[1.0,1.0],[1.0,2.0],[2.0,2.0],[2.0,1.0]],
+        [[3.0,3.0],[3.0,4.0],[4.0,4.0],[4.0,3.0]],
+      ]
+    end
+    it 'raises an error with _1.map' do
+      expect do
+      map = path('_1.map')
+      subject.load map
+      end.to raise_error Sheep::NUMBEROF_OBJECTS_NOT_MATCHED
+    end
+    it 'raises an error with _2.map' do
+      expect do
+      map = path('_2.map')
+      subject.load map
+      end.to raise_error Sheep::NUMBEROF_POINTS_NOT_MATCHED
+    end
+  end
 end
