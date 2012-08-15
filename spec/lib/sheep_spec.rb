76d1755c5d134d8bdfb08d6a568c388ae85f46d1
Minwoo Lee <ermaker@gmail.com>
2012-08-15 15:33:39 +0900

    Modify the name of path method to fixture

--- spec/lib/sheep_spec.rb
+++ spec/lib/sheep_spec.rb
@@ -1,6 +1,6 @@
 require 'sheep'
 
-def path filename
+def fixture filename
   File.join(File.dirname(__FILE__), 'fixtures', filename)
 end
 
@@ -11,17 +11,17 @@ end
 describe Sheep do
   context '#load' do
     it 'reads map with 1.map' do
-      map = path('1.map')
+      map = fixture('1.map')
       subject.load map
       subject.objects.should == []
     end
     it 'reads map with 2.map' do
-      map = path('2.map')
+      map = fixture('2.map')
       subject.load map
       subject.objects.should == [[[1.0,1.0],[2.0,1.0],[2.0,2.0],[1.0,2.0]]]
     end
     it 'reads map with 3.map' do
-      map = path('3.map')
+      map = fixture('3.map')
       subject.load map
       subject.objects.should == [
         [[1.0,1.0],[2.0,1.0],[2.0,2.0],[1.0,2.0]],
@@ -30,13 +30,13 @@ describe Sheep do
     end
     it 'raises an error with _1.map' do
       expect do
-      map = path('_1.map')
+      map = fixture('_1.map')
       subject.load map
       end.to raise_error Sheep::NUMBEROF_OBJECTS_NOT_MATCHED
     end
     it 'raises an error with _2.map' do
       expect do
-      map = path('_2.map')
+      map = fixture('_2.map')
       subject.load map
       end.to raise_error Sheep::NUMBEROF_POINTS_NOT_MATCHED
     end
