63eb962b12712b4cb09b8bcd63cd5aece4e6189b
Minwoo Lee <ermaker@gmail.com>
2012-08-21 17:27:43 +0900

    Add Algorithms::Naive#query

--- lib/algorithms/naive.rb
+++ lib/algorithms/naive.rb
@@ -1,4 +1,25 @@
+require 'geometry'
+require 'ext/geometry'
+
 module Algorithms
   class Naive
+    attr_accessor :objects
+
+    def initialize objects
+      @objects = objects
+    end
+
+    def query minx, miny, maxx, maxy
+      query_area = Polygon [
+        Point(minx, miny),
+        Point(maxx, miny),
+        Point(maxx, maxy),
+        Point(minx, maxy),
+      ]
+      @objects.count do |obj|
+        Polygon(obj.map{|p|Geometry::Point.new_by_array(p)}).
+          counting?(query_area)
+      end
+    end
   end
 end
--- spec/lib/algorithms/naive_spec.rb
+++ spec/lib/algorithms/naive_spec.rb
@@ -1,4 +1,21 @@
 require 'algorithms/naive'
 
 describe Algorithms::Naive do
+  subject do 
+    described_class.new [
+      [[2.0,1.0],[4.0,1.0],[4.0,6.0],[2.0,6.0]],
+      [[1.0,5.0],[5.0,5.0],[5.0,7.0],[1.0,7.0]],
+      [[4.0,2.0],[5.0,2.0],[5.0,3.0],[4.0,3.0]],
+    ]
+  end
+  context '#query' do
+    it 'works' do
+      subject.query(0.0, 0.0, 6.0, 8.0).should == 3
+      subject.query(4.0, 2.0, 5.0, 3.0).should == 1
+      subject.query(3.0, 5.0, 4.0, 6.0).should == 2
+      subject.query(3.0, 4.0, 5.0, 6.0).should == 2
+      subject.query(4.0, 4.0, 5.0, 5.0).should == 0
+      subject.query(3.0, 2.0, 5.0, 6.0).should == 3
+    end
+  end
 end
