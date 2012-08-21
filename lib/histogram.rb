4077d1b30e3eda3f4bb54d895c993979fb2ff76c
Minwoo Lee <ermaker@gmail.com>
2012-08-21 14:44:23 +0900

    Add Histogram#bounds

--- lib/histogram.rb
+++ lib/histogram.rb
@@ -1,2 +1,35 @@
 class Histogram
+  attr_accessor :data, :minx, :miny, :maxx, :maxy, :stepx, :stepy
+
+  def initialize data, minx, miny, maxx, maxy, stepx, stepy
+    @data = data
+    @minx = minx
+    @miny = miny
+    @maxx = maxx
+    @maxy = maxy
+    @stepx = stepx
+    @stepy = stepy
+  end
+
+  def bounds qminx, qminy, qmaxx, qmaxy
+    lminx = (miny..maxy).
+      step((maxy-miny)/stepx).map.with_index.select {|y,| y>=qminy}.min[1]
+    lminy = (minx..maxx).
+      step((maxx-minx)/stepy).map.with_index.select {|x,| x>=qminx}.min[1]
+    lmaxx = (miny..maxy).
+      step((maxy-miny)/stepx).map.with_index.select {|y,| y<=qmaxy}.max[1]
+    lmaxy = (minx..maxx).
+      step((maxx-minx)/stepy).map.with_index.select {|x,| x<=qmaxx}.max[1]
+
+    uminx = (miny..maxy).
+      step((maxy-miny)/stepx).map.with_index.select {|y,| y<=qminy}.max[1]
+    uminy = (minx..maxx).
+      step((maxx-minx)/stepy).map.with_index.select {|x,| x<=qminx}.max[1]
+    umaxx = (miny..maxy).
+      step((maxy-miny)/stepx).map.with_index.select {|y,| y>=qmaxy}.min[1]
+    umaxy = (minx..maxx).
+      step((maxx-minx)/stepy).map.with_index.select {|x,| x>=qmaxx}.min[1]
+
+    return [[lminx, lminy, lmaxx, lmaxy], [uminx, uminy, umaxx, umaxy]]
+  end
 end
--- spec/lib/histogram_spec.rb
+++ spec/lib/histogram_spec.rb
@@ -1,4 +1,42 @@
 require 'histogram'
 
 describe Histogram do
+  subject do
+    Histogram.new [
+      [0, 0, 1, 0, 1],
+      [0, 0, -1, 0, -1],
+      [1, -1, 2, -1, 2],
+      [-1, 1, -2, 1 ,-1],
+      [1, -1, 2, -1, 1],
+    ], 0.0, 0.0, 6.0, 8.0, 2, 2
+  end
+
+  context '#bounds' do
+    it 'works' do
+      subject.bounds(0.0, 0.0, 6.0, 8.0).should == [
+        [0, 0, 2, 2],
+        [0, 0, 2, 2],
+      ]
+      subject.bounds(4.0, 2.0, 5.0, 3.0).should == [
+        [1, 2, 0, 1],
+        [0, 1, 1, 2],
+      ]
+      subject.bounds(3.0, 5.0, 4.0, 6.0).should == [
+        [2, 1, 1, 1],
+        [1, 1, 2, 2],
+      ]
+      subject.bounds(3.0, 4.0, 5.0, 6.0).should == [
+        [1, 1, 1, 1],
+        [1, 1, 2, 2],
+      ]
+      subject.bounds(4.0, 4.0, 5.0, 5.0).should == [
+        [1, 2, 1, 1],
+        [1, 1, 2, 2],
+      ]
+      subject.bounds(3.0, 2.0, 5.0, 6.0).should == [
+        [1, 1, 1, 1],
+        [0, 1, 2, 2],
+      ]
+    end
+  end
 end
