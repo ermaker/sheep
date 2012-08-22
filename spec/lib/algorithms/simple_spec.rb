254675338618193341f09588933841fec4628b8e
Minwoo Lee <ermaker@gmail.com>
2012-08-22 16:32:33 +0900

    Add spec

--- spec/lib/algorithms/simple_spec.rb
+++ spec/lib/algorithms/simple_spec.rb
@@ -38,3 +38,42 @@ describe Algorithms::Simple do
     end
   end
 end
+
+describe Algorithms::Simple, 'with special objects' do
+  subject do
+    sheep = Sheep.new
+    sheep.objects = [
+      [[2.0,1.0],[4.0,1.0],[4.0,6.0],[2.0,6.0]],
+      [[1.0,5.0],[5.0,5.0],[5.0,7.0],[1.0,7.0]],
+      [[4.0,2.0],[5.0,2.0],[5.0,3.0],[4.0,3.0]],
+    ]
+    described_class.new(sheep, 3, 2, 2)
+  end
+
+  context '#histograms' do
+    it 'works' do
+      subject.histograms.should have(3).items
+      subject.histograms[0].query(0.0, 0.0, 6.0, 8.0).should == 1
+      subject.histograms[1].query(0.0, 0.0, 6.0, 8.0).should == 1
+      subject.histograms[2].query(0.0, 0.0, 6.0, 8.0).should == 1
+    end
+  end
+
+  context '#query' do
+    it 'works' do
+      subject.query(0.0, 0.0, 6.0, 8.0).should == 3
+      subject.query(4.0, 2.0, 5.0, 3.0).should == 1.0
+      subject.query(3.0, 5.0, 4.0, 6.0).should == 0.9
+      subject.query(3.0, 4.0, 5.0, 6.0).should == 2.6
+      subject.query(4.0, 4.0, 5.0, 5.0).should == 0.0
+      subject.query(3.0, 2.0, 5.0, 6.0).should == 3.0
+    end
+
+    it 'works with special cases' do
+      subject.query(-100.0, -100.0, 100.0, 100.0).should == 3
+      subject.query(-100.0, -100.0, -50.0, -50.0).should == 0
+      subject.query(3.0, -10.0, 10.0, 50.0).should == 3
+      subject.query(7.0, 9.0, 9.0, 11.0).should == 0
+    end
+  end
+end
