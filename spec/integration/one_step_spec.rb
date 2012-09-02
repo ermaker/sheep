1c05ece567ea688f2d8fbf494f51a37351f99e50
Minwoo Lee <ermaker@gmail.com>
2012-09-02 17:15:16 +0900

    Add one step spec

---
+++ spec/integration/one_step_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+require 'query'
+require 'farm'
+require 'sheep'
+require 'algorithms/naive'
+require 'algorithms/histogram'
+
+describe 'One step' do
+  it 'works on Algorithms::Naive' do
+    farm = Farm.new
+    farm.sheep = Sheep.new
+    farm.sheep.load fixture('3.map')
+    area = 0.01
+    queries = 10.times.map { Query.generate farm.sheep, area }
+    farm.set_algorithm Algorithms::Naive
+    result = queries.map do |query|
+      farm.query *query
+    end
+  end
+
+  it 'works on Algorithms::Histogram' do
+    farm = Farm.new
+    farm.sheep = Sheep.new
+    farm.sheep.load fixture('3.map')
+    area = 0.01
+    queries = 10.times.map { Query.generate farm.sheep, area }
+    farm.set_algorithm Algorithms::Histogram, 4, 4
+    result = queries.map do |query|
+      farm.query *query
+    end
+  end
+end
