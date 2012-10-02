b0e47ee4dd76c718b59951d7494ade5fd6c3efd2
Minwoo Lee <ermaker@gmail.com>
2012-10-02 16:58:09 +0900

    Modify spec

--- spec/lib/kdtree/leaf_node_spec.rb
+++ spec/lib/kdtree/leaf_node_spec.rb
@@ -1,20 +1,23 @@
 require 'kdtree/leaf_node'
 
 describe Kdtree::LeafNode do
+  before do
+    @objects = [
+      [[2.0,1.0],[4.0,1.0],[4.0,6.0],[2.0,6.0]],
+      [[1.0,5.0],[5.0,5.0],[5.0,7.0],[1.0,7.0]],
+      [[4.0,2.0],[5.0,2.0],[5.0,3.0],[4.0,3.0]],
+    ]
+    @leaf_nodes = @objects.map {|object| described_class.new(object)}
+  end
+
   context '#mbr' do
     it 'works' do
-      objects = [
-        [[2.0,1.0],[4.0,1.0],[4.0,6.0],[2.0,6.0]],
-        [[1.0,5.0],[5.0,5.0],[5.0,7.0],[1.0,7.0]],
-        [[4.0,2.0],[5.0,2.0],[5.0,3.0],[4.0,3.0]],
-      ]
       mbr = [
         [2.0, 1.0, 4.0, 6.0],
         [1.0, 5.0, 5.0, 7.0],
         [4.0, 2.0, 5.0, 3.0],
       ]
-      objects.zip(mbr).map do |object,mbr|
-        leaf_node = described_class.new(object)
+      @leaf_nodes.zip(mbr).map do |leaf_node,mbr|
         leaf_node.calculate_mbr
         leaf_node.mbr.should == mbr
       end
