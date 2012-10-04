a752dfacb3d5f7e96419da469b3146c3ebdcc74f
Minwoo Lee <ermaker@gmail.com>
2012-10-05 00:55:48 +0900

    Optimize

--- lib/kdtree/leaf_node.rb
+++ lib/kdtree/leaf_node.rb
@@ -24,10 +24,11 @@ module Kdtree
     def query minx, miny, maxx, maxy
       return 0 unless (mbr[0] > minx ? mbr[0] : minx) <= (mbr[2] < maxx ? mbr[2] : maxx) and (mbr[1] > miny ? mbr[1] : miny) <= (mbr[3] < maxy ? mbr[3] : maxy)
 
-      clipper = Clipper::Clipper.new
-      clipper.add_subject_polygon(@object)
-      clipper.add_clip_polygon([[minx,miny],[maxx,miny],[maxx,maxy],[minx,maxy]])
-      clipper.intersection.empty? ? 0 : 1
+      $clipper ||= Clipper::Clipper.new
+      $clipper.clear!
+      $clipper.add_subject_polygon(@object)
+      $clipper.add_clip_polygon([[minx,miny],[maxx,miny],[maxx,maxy],[minx,maxy]])
+      $clipper.intersection.empty? ? 0 : 1
     end
   end
 end
