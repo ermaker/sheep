0b66768fa91796a821740638f042228fc1dfe05e
Minwoo Lee <ermaker@gmail.com>
2012-10-04 17:21:56 +0900

    Modify the algorithm of Kdtree::Node#query

--- lib/kdtree/node.rb
+++ lib/kdtree/node.rb
@@ -19,19 +19,7 @@ module Kdtree
     end
 
     def query minx, miny, maxx, maxy
-      query_area = Polygon [
-        Point(minx, miny),
-        Point(maxx, miny),
-        Point(maxx, maxy),
-        Point(minx, maxy),
-      ]
-      mbr_area = Polygon [
-        Point(mbr[0], mbr[1]),
-        Point(mbr[2], mbr[1]),
-        Point(mbr[2], mbr[3]),
-        Point(mbr[0], mbr[3]),
-      ]
-      return 0 unless mbr_area.counting?(query_area)
+      return 0 unless [mbr[0], minx].max <= [mbr[2], maxx].min and [mbr[1], miny].max <= [mbr[3], maxy].min
 
       return @nodes.map {|node| node.query minx, miny, maxx, maxy}.inject(:+)
     end
