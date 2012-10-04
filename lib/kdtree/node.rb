a30c9eaf021e85973e8e83245c1087f123f6db27
Minwoo Lee <ermaker@gmail.com>
2012-10-05 00:34:07 +0900

    Optimize

--- lib/kdtree/node.rb
+++ lib/kdtree/node.rb
@@ -21,9 +21,13 @@ module Kdtree
     def query minx, miny, maxx, maxy
       return 0 unless (mbr[0] > minx ? mbr[0] : minx) <= (mbr[2] < maxx ? mbr[2] : maxx) and (mbr[1] > miny ? mbr[1] : miny) <= (mbr[3] < maxy ? mbr[3] : maxy)
 
+      return @nodes[0].query(minx, miny, maxx, maxy)+
+        @nodes[1].query(minx, miny, maxx, maxy)
+=begin
       return @nodes.inject(0) do |result,node|
         result += node.query minx, miny, maxx, maxy
       end
+=end
     end
   end
 end
